bf3f0d14cdcfc4b8025095dc3bb974846e3d9064
Gio <modegiorgi@gmail.com>
2019-06-30 13:03:38 +0200

    returning top n results

--- src/main/java/com/giorgimode/WebCrawler.java
+++ src/main/java/com/giorgimode/WebCrawler.java
@@ -6,44 +6,59 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.util.Comparator.reverseOrder;
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
 
 
 public class WebCrawler {
 
     private static final String GOOGLE_RESULT_URL_PATTERN1 = "<a href=\"/url?q=";
     private static final String GOOGLE_RESULT_URL_PATTERN2 = "\">";
-    private static final Pattern GOOGLE_RESULT_URL_PATTERN = Pattern.compile(Pattern.quote(GOOGLE_RESULT_URL_PATTERN1) + "(.*?)" + Pattern.quote(GOOGLE_RESULT_URL_PATTERN2));
+    private static final Pattern GOOGLE_RESULT_URL_PATTERN = Pattern.compile(Pattern.quote(GOOGLE_RESULT_URL_PATTERN1)
+            + "(.*?)" + Pattern.quote(GOOGLE_RESULT_URL_PATTERN2));
     private static final Pattern JS_LIBRARY_PATTERN = Pattern.compile("\\b(?<![<>][\\s]|\\w)[\\w-]*?\\.js\\b");
+    private static final String USER_AGENT = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
+    private static final String QUERY_FORMAT = "https://www.google.com/search?q=%s&num=10";
+    private static final int TOP_LIBRARY_COUNT = 5;
 
-    public static void crawl(String searchTerm) throws Exception {
-        String query = "https://www.google.com/search?q=" + searchTerm + "&num=10";
-
-        parseLinks(getSearchContent(query)).forEach(returnUrl -> {
-            System.out.println(returnUrl);
-            try {
-                List<String> strings = parseLibraries(getSearchContent(returnUrl));
-                System.out.println(strings);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-
-        });
+    static Map<String, Long> retrieveLibraries(String searchTerm) {
+        return getHtmlContent(String.format(QUERY_FORMAT, searchTerm))
+                .map(WebCrawler::parseLinks)
+                .stream()
+                .flatMap(Set::stream)
+                .map(WebCrawler::getHtmlContent)
+                .flatMap(Optional::stream)
+                .map(WebCrawler::parseLibraries)
+                .flatMap(List::stream)
+                .peek(System.out::println)
+                .collect(groupingBy(identity(), counting()))
+                .entrySet()
+                .stream()
+                .sorted(comparingByValue(reverseOrder()))
+                .limit(TOP_LIBRARY_COUNT)
+                .collect(toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (v1, v2) -> v1,
+                        LinkedHashMap::new
+                ));
     }
 
-    private static String getSearchContent(String path) throws Exception {
-        final String agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
-        URL url = new URL(path);
-        final URLConnection connection = url.openConnection();
-
-        connection.setRequestProperty("User-Agent", agent);
-        final InputStream stream = connection.getInputStream();
-        return getString(stream);
+    private static Optional<String> getHtmlContent(String path) {
+        try {
+            URLConnection connection = new URL(path).openConnection();
+            connection.setRequestProperty("User-Agent", USER_AGENT);
+            return Optional.of(getString(connection.getInputStream()));
+        } catch (IOException e) {
+            // exceptions should be logged/monitored/not ignored in production
+            return Optional.empty();
+        }
     }
 
     private static String getString(InputStream is) {
@@ -67,7 +82,7 @@ public class WebCrawler {
         return sb.toString();
     }
 
-    private static Set<String> parseLinks(final String html) {
+    private static Set<String> parseLinks(String html) {
         Set<String> result = new HashSet<>();
         Matcher matcher = GOOGLE_RESULT_URL_PATTERN.matcher(html);
 
@@ -82,7 +97,7 @@ public class WebCrawler {
         return result;
     }
 
-    private static List<String> parseLibraries(final String html) {
+    private static List<String> parseLibraries(String html) {
         List<String> result = new ArrayList<>();
         Matcher matcher = JS_LIBRARY_PATTERN.matcher(html);
 
--- src/main/java/com/giorgimode/WebCrawlerInit.java
+++ src/main/java/com/giorgimode/WebCrawlerInit.java
@@ -1,19 +1,22 @@
 package com.giorgimode;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class WebCrawlerInit {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
         System.out.println("Please enter a search string: ");
-        Scanner scanner = new Scanner(System.in);
         String searchString = scanner.nextLine();
         while (searchString == null || searchString.trim().isBlank()) {
             System.out.println("Blank text is not allowed");
             searchString = scanner.nextLine();
         }
         System.out.println("You entered " + searchString);
-        WebCrawler.crawl(searchString);
+        Map<String, Long> libraryFrequencyMap = WebCrawler.retrieveLibraries(searchString);
+        libraryFrequencyMap.forEach((library, frequency) ->
+                System.out.println("Library " + library + " has been used " + frequency + " times"));
 
     }
 }
