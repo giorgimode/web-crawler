c77aee709cd86068752398bd2004f549513a1496
Gio <modegiorgi@gmail.com>
2019-06-30 12:41:56 +0200

    updated regex for retrieving libraries

--- src/main/java/com/giorgimode/WebCrawler.java
+++ src/main/java/com/giorgimode/WebCrawler.java
@@ -7,18 +7,33 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-
 public class WebCrawler {
+
+    private static final String GOOGLE_RESULT_URL_PATTERN1 = "<a href=\"/url?q=";
+    private static final String GOOGLE_RESULT_URL_PATTERN2 = "\">";
+    private static final Pattern GOOGLE_RESULT_URL_PATTERN = Pattern.compile(Pattern.quote(GOOGLE_RESULT_URL_PATTERN1) + "(.*?)" + Pattern.quote(GOOGLE_RESULT_URL_PATTERN2));
+    private static final Pattern JS_LIBRARY_PATTERN = Pattern.compile("\\b(?<![<>][\\s]|\\w)[\\w-]*?\\.js\\b");
 
     public static void crawl(String searchTerm) throws Exception {
         String query = "https://www.google.com/search?q=" + searchTerm + "&num=10";
 
-        parseLinks(getSearchContent(query)).forEach(System.out::println);
+        parseLinks(getSearchContent(query)).forEach(returnUrl -> {
+            System.out.println(returnUrl);
+            try {
+                List<String> strings = parseLibraries(getSearchContent(returnUrl));
+                System.out.println(strings);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        });
     }
 
     private static String getSearchContent(String path) throws Exception {
@@ -52,20 +67,28 @@ public class WebCrawler {
         return sb.toString();
     }
 
-    private static List<String> parseLinks(final String html) {
-        List<String> result = new ArrayList<>();
-        String pattern1 = "<a href=\"/url?q=";
-        String pattern2 = "\">";
-        Pattern p = Pattern.compile(Pattern.quote(pattern1) + "(.*?)" + Pattern.quote(pattern2));
-        Matcher m = p.matcher(html);
+    private static Set<String> parseLinks(final String html) {
+        Set<String> result = new HashSet<>();
+        Matcher matcher = GOOGLE_RESULT_URL_PATTERN.matcher(html);
 
-        while (m.find()) {
-            String domainName = m.group(0).trim();
+        while (matcher.find()) {
+            String domainName = matcher.group(0).trim();
 
-            domainName = domainName.substring(domainName.indexOf("/url?q=") + 7);
+            domainName = domainName.substring(domainName.indexOf(GOOGLE_RESULT_URL_PATTERN1) + GOOGLE_RESULT_URL_PATTERN1.length());
             domainName = domainName.substring(0, domainName.indexOf("&amp;"));
 
             result.add(domainName);
+        }
+        return result;
+    }
+
+    private static List<String> parseLibraries(final String html) {
+        List<String> result = new ArrayList<>();
+        Matcher matcher = JS_LIBRARY_PATTERN.matcher(html);
+
+        while (matcher.find()) {
+            String library = matcher.group(0).trim();
+            result.add(library);
         }
         return result;
     }
